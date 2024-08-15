b2c078cccce80beb0d05d040622f0b9bab4feeae
Parth Kapoor <pkapoor_be22@thapar.edu>
2024-08-16 00:08:03 +0530

    Minor Changes

--- client.c
+++ client.c
@@ -43,7 +43,7 @@ int main(){
     printf("Recived Info : %s", recvline);
 
   }else{
-    printf("Connection couldn't be established");
+    printf("Connection couldn't be established : %d" , connection);
   }
 
 }
